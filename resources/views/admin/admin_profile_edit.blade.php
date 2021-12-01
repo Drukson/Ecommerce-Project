d2bc9500e4d96038fd855ea67a3249a88c0510ed
wangzin <wangzin53@gmail.com>
2021-12-01 21:10:17 +0600

    changes

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -48,24 +48,101 @@
                                         <div class="form-group">
                                             <h5>Dzongkhag <span class="text-danger">*</span></h5>
                                             <div class="controls">
-                                                <input type="text" name="dzongkhag_id" value="{{$editAdminProfile->dzongkhag->dzongkhag_name}}" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
+                                                <select name="dzongkhag_id" onchange="getgeowg('dzongkhag_id')" id="dzongkhag_id" class="form-control">
+                                                    <option value="" selected="" disabled="">Select Dzongkhag</option>
+                                                    @foreach($dzongkhag as $data)
+                                                        <option value="{{$data->id}}">{{$data->dzongkhag_name}}</option>
+                                                    @endforeach
+                                                </select>
+                                                @error('dzongkhag_id')
+                                                    <span class="text-danger">{{$message}}</span>
+                                                @enderror
+                                                {{-- <input type="text" name="dzongkhag_id" value="{{$editAdminProfile->dzongkhag->dzongkhag_name}}" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div> --}}
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <h5>Gewog <span class="text-danger">*</span></h5>
                                             <div class="controls">
-                                                <input type="text" name="gewog_id" value="{{$editAdminProfile->gewog->gewog_name}}" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
+                                                <select name="gewog_id" onchange="getvillage('gewog_id')" id="gewog_id" class="form-control">
+                                                    <option value="" selected="" disabled="">Select Gewog</option>
+                                                </select>
+                                                @error('gewog_id')
+                                                <span class="text-danger">{{$message}}</span>
+                                                @enderror
+                                                {{-- <input type="text" name="gewog_id" value="{{$editAdminProfile->gewog->gewog_name}}" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div> --}}
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <h5>Village <span class="text-danger">*</span></h5>
                                             <div class="controls">
-                                                <input type="text" name="village" value="{{$editAdminProfile->village->village_name}}" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div></div>
+                                                <select name="village_id" id="village_id" class="form-control">
+                                                    <option value="" selected="" disabled="">Select Village</option>
+                                                </select>
+                                                {{-- <input type="text" name="village" value="{{$editAdminProfile->village->village_name}}" class="form-control" required="" data-validation-required-message="This field is required"> <div class="help-block"></div> --}}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                @php
+                                    if(isset($editAdminProfile->seller)){
+                                @endphp
+                                <script>
+                                    if('{{$editAdminProfile->seller->dzongkhag_id}}'!=null && '{{$editAdminProfile->seller->dzongkhag_id}}'!=''){
+                                        $('#dzongkhag_id').val('{{$editAdminProfile->seller->dzongkhag_id}}');
+                                    }
+                                    getgeowg('{{$editAdminProfile->seller->dzongkhag_id}}','{{$editAdminProfile->seller->gewog_id}}');
+                                    getvillage('{{$editAdminProfile->seller->dzongkhag_id}}','{{$editAdminProfile->seller->gewog_id}}');
+                                </script>
+                                @php
+                                    }
+                                @endphp
+                                <script>
+                                    function getgeowg(dzoId,gewogId){
+                                        if(dzoId=="dzongkhag_id"){
+                                            dzoId=$('#dzongkhag_id').val();
+                                        }
+                                        $.ajax({
+                                            url: "{{  url('/dzongkhag/gewog/ajax') }}/"+dzoId,
+                                            type:"GET",
+                                            dataType:"json",
+                                            success:function(data) {
+                                                var d =$('select[name="gewog_id"]').empty();
+                                                $('select[name="gewog_id"]').append('<option value="" selected="" disabled="">Select gewog</option>');
+                                                $.each(data, function(key, value){
+                                                    $('select[name="gewog_id"]').append('<option value="'+ value.id +'">' + value.gewog_name + '</option>');
+                                                });
+                                                if(gewogId!=null && gewogId!=undefined && gewogId!=""){
+                                                    $('#gewog_id').val(gewogId);
+                                                }
+                                            },
+                                        });
+                                    }
+
+                                    function getvillage(gewog_id,village_id){
+                                        if(gewog_id=="gewog_id"){
+                                            gewog_id=$('#gewog_id').val();
+                                        }
+                                        $.ajax({
+                                            url: "{{  url('/dzongkhag/village/ajax') }}/"+gewog_id,
+                                            type:"GET",
+                                            dataType:"json",
+                                            success:function(data) {
+                                                var d =$('select[name="village_id"]').empty();
+                                                $('select[name="village_id"]').append('<option value="" selected="" disabled="">Select Village</option>');
+                                                $.each(data, function(key, value){
+                                                    $('select[name="village_id"]').append('<option value="'+ value.id +'">' + value.village_name + '</option>');
+                                                });
+                                                if(village_id!=null && village_id!=undefined && village_id!=""){
+                                                    $('#village_id').val(village_id);
+                                                }
+                                            },
+                                        });
+                                    }
+                                </script>
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
